0542c01f7d23d04581f2775a265fd2bfd467a52f
robharmer3 <rob.harmer3@gmail.com>
2025-11-09 21:52:18 +0000

    contact logos added. bio section content updated. bio CSS updated but not complete

--- src/Sections/Bio.jsx
+++ src/Sections/Bio.jsx
@@ -1,24 +1,66 @@
 export default function Bio() {
   return (
-    <div className="bio">
+    <div>
       <h2>Bio</h2>
-      <p className="bio-list">
-        Hello, my name is Rob Harmer and I am a Software Developer in search of
-        my first software development role.
-      </p>
-      <p>
-        I hold a Mechanical Engineering degree and several years of experience{" "}
-      </p>
-      <ul className="bio-list">
-        <li>
-          💻 Software Developer. Back-End (JavaScript, Node.js). Front-End
-          (React, React Native, CSS)
-        </li>
-        <li>🎓 Northcoders Graduate. January 2025 Cohort.</li>
-        <li>📜 Mechanical Engineering Degree from the University of Bolton.</li>
-        <li>💏 Husband to my beautiful wife. Father to my amazing daughter.</li>
-        <li>😸 Slightly cat mad! I have 3!!!</li>
-      </ul>
+      <div className="bio">
+        <div className="bio-list">
+          <img
+            src="/src/assets/profile-pic.jpg"
+            alt=""
+            className="profile-pic"
+          />
+        </div>
+        <div className="bio-list">
+          <h3>Intro...</h3>
+          <p>
+            Hello, I'm Rob and I am a Software Developer in search of my first
+            software development role.
+            <br />I have recently graduated from Northcoders' JavaScript
+            bootcamp.
+            <br />
+            While on the bootcamp I gain experience with JavaScript, Node.js and
+            React.
+            <br />
+            As well as practiced Test-Driven Development, Agile methodology, and
+            pair programming.
+            <br />I hold a Mechanical Engineering degree and several years of
+            experience{" "}
+          </p>
+        </div>
+        <div className="bio-list">
+          <h3>Previosuly...</h3>
+          <p>
+            Before switching careers to Software Development, I was Project and
+            Desgin Engineer <br />
+            with a degree in Mechanical Engineering from the University of
+            Bolton.
+            <br />I also have several years in a wide range of industries, most
+            recently the automotive sector.
+          </p>
+        </div>
+        <div className="bio-list">
+          <h3>Why Software Development?</h3>
+          <ul>
+            <li>because I enjoy solving problems in a creative ways.</li>
+            <li>because I want to feel like I'm making a real difference.</li>
+            <li>
+              because I want to work with cutting-edge technologies and broaden
+              my knowledge of the wider tech industry.
+            </li>
+            <li>because I want to be the best!</li>
+          </ul>
+        </div>
+        <div className="bio-list">
+          <h3>Fun Facts...</h3>
+          <ul>
+            <li>
+              💏 Husband to my beautiful wife. Father to my amazing daughter.
+            </li>
+            <li>😸 Slightly cat mad! I have 3!!!</li>
+            <li>🏑Keen hockey player.</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Sections/Contact.jsx
+++ src/Sections/Contact.jsx
@@ -4,20 +4,24 @@ export default function Contact() {
       <h2>Contact</h2>
 
       <div className="contact-container">
+        <img src="/src/assets/linkedin.png" alt="linkedin logo" />
         <p className="contact-title">LinkedIn</p>
         <p>https://www.linkedin.com/in/rmharmer39/</p>
       </div>
       <div className="contact-container">
+        <img src="/src/assets/github.png" alt="github logo" />
         <p className="contact-title">GitHub</p>
         <p>https://github.com/robharmer3</p>
       </div>
       <div className="contact-container">
+        <img src="/src/assets/email.png" alt="email logo" />
         <p className="contact-title">Email</p>
-        <p>rob.harmer3@gmail.com</p>
+        <p className="contact-content">rob.harmer3@gmail.com</p>
       </div>
       <div className="contact-container">
+        <img src="/src/assets/location.png" alt="location logo" />
         <p className="contact-title">Location</p>
-        <p>Bolton, England</p>
+        <p className="contact-content">Bolton, England</p>
       </div>
     </div>
   );
